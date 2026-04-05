a980b23ee81420a8e8846b4a758cb8c4163729e7
vineet190190 <bishta243@gmail.com>
2026-04-05 22:51:36 +0530

    added api

--- src/store/appointments/saga.ts
+++ src/store/appointments/saga.ts
@@ -1,10 +1,5 @@
-import { call, put, takeLatest, all, select } from "redux-saga/effects";
-import {
-  GET_REQUEST,
-  POST_REQUEST,
-  PUT_REQUEST,
-  DELETE_REQUEST,
-} from "../../services/apiHelper";
+import { call, put, takeLatest, all, select } from 'redux-saga/effects';
+import { GET_REQUEST, POST_REQUEST, PUT_REQUEST, DELETE_REQUEST } from '../../services/apiHelper';
 import {
   fetchAppointmentsRequest,
   fetchAppointmentsSuccess,
@@ -19,50 +14,49 @@ import {
   deleteAppointmentSuccess,
   deleteAppointmentFailure,
   showSnackbar,
-} from "./slice";
-import { PayloadAction } from "@reduxjs/toolkit";
-import { Appointment } from "./slice";
-import API_ENDPOINTS from "../../services/apiEndPoint";
-import { selectAppointments } from "./selectors";
-import {
-  isOverlapping,
-  isWithinBusinessHours,
-} from "../../utils/appointment.utils";
+} from './slice';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { Appointment } from './slice';
+import API_ENDPOINTS from '../../services/apiEndPoint';
+import { selectAppointments } from './selectors';
+import { isOverlapping, isWithinBusinessHours } from '../../utils/appointment.utils';
+import { hideLoader, showLoader } from '../global/slice';
 
 function* fetchAppointmentsSaga() {
   try {
+    yield put(showLoader());
     const response: { data: Appointment[] } = yield call(
       GET_REQUEST,
-      API_ENDPOINTS.APPOINTMENTS.GET_ALL,
+      API_ENDPOINTS.APPOINTMENTS.GET_ALL
     );
 
     yield put(fetchAppointmentsSuccess(response.data));
   } catch (error: any) {
-    yield put(fetchAppointmentsFailure("Unable to fetch appointments"));
+    yield put(fetchAppointmentsFailure('Unable to fetch appointments'));
     yield put(
       showSnackbar({
-        message: error?.message || "Failed to fetch",
-        severity: "error",
-      }),
+        message: error?.message || 'Failed to fetch',
+        severity: 'error',
+      })
     );
+  } finally {
+    yield put(hideLoader());
   }
 }
 
 function* addAppointmentSaga(action: PayloadAction<Appointment>) {
   try {
-    const existingAppointments: Appointment[] =
-      yield select(selectAppointments);
+    yield put(showLoader());
+    const existingAppointments: Appointment[] = yield select(selectAppointments);
 
-    const hasConflict = existingAppointments.some((appt) =>
-      isOverlapping(action.payload, appt),
-    );
+    const hasConflict = existingAppointments.some((appt) => isOverlapping(action.payload, appt));
 
     if (hasConflict) {
       yield put(
         showSnackbar({
-          message: "Doctor already has an appointment at this time",
-          severity: "error",
-        }),
+          message: 'Doctor already has an appointment at this time',
+          severity: 'error',
+        })
       );
       return;
     }
@@ -71,54 +65,54 @@ function* addAppointmentSaga(action: PayloadAction<Appointment>) {
       yield put(
         showSnackbar({
           message: `Appointments can only be scheduled between 09:00 and 18:00`,
-          severity: "error",
-        }),
+          severity: 'error',
+        })
       );
       return;
     }
 
-    console.log("payload", action.payload);
+    console.log('payload', action.payload);
     const response: { data: Appointment } = yield call(
       POST_REQUEST,
       API_ENDPOINTS.APPOINTMENTS.CREATE,
-      action.payload,
+      action.payload
     );
-    console.log("response", response);
+    console.log('response', response);
 
     yield put(addAppointmentSuccess(response.data));
 
     yield put(
       showSnackbar({
-        message: "Appointment added successfully",
-        severity: "success",
-      }),
+        message: 'Appointment added successfully',
+        severity: 'success',
+      })
     );
   } catch (error: any) {
-    console.log("error-------", error);
-    yield put(addAppointmentFailure("Failed to add appointment"));
+    console.log('error-------', error);
+    yield put(addAppointmentFailure('Failed to add appointment'));
     yield put(
       showSnackbar({
-        message: error?.message || "Failed to create appointment",
-        severity: "error",
-      }),
+        message: error?.message || 'Failed to create appointment',
+        severity: 'error',
+      })
     );
+  } finally {
+    yield put(hideLoader());
   }
 }
 
 function* updateAppointmentSaga(action: PayloadAction<Appointment>) {
   try {
-    const existingAppointments: Appointment[] =
-      yield select(selectAppointments);
+    yield put(showLoader());
+    const existingAppointments: Appointment[] = yield select(selectAppointments);
 
-    const hasConflict = existingAppointments.some((appt) =>
-      isOverlapping(action.payload, appt),
-    );
+    const hasConflict = existingAppointments.some((appt) => isOverlapping(action.payload, appt));
     if (hasConflict) {
       yield put(
         showSnackbar({
-          message: "Doctor already has an appointment at this time",
-          severity: "error",
-        }),
+          message: 'Doctor already has an appointment at this time',
+          severity: 'error',
+        })
       );
       return;
     }
@@ -127,56 +121,56 @@ function* updateAppointmentSaga(action: PayloadAction<Appointment>) {
       yield put(
         showSnackbar({
           message: `Appointments can only be scheduled between 09:00 and 18:00`,
-          severity: "error",
-        }),
+          severity: 'error',
+        })
       );
       return;
     }
     const response: { data: Appointment } = yield call(
       PUT_REQUEST,
       API_ENDPOINTS.APPOINTMENTS.UPDATE(action.payload.id),
-      action.payload,
+      action.payload
     );
 
     yield put(updateAppointmentSuccess(response.data));
     yield put(
       showSnackbar({
-        message: "Appointment updated successfully",
-        severity: "success",
-      }),
+        message: 'Appointment updated successfully',
+        severity: 'success',
+      })
     );
   } catch (error: any) {
-    yield put(updateAppointmentFailure("Failed to update appointment"));
+    yield put(updateAppointmentFailure('Failed to update appointment'));
     yield put(
       showSnackbar({
-        message: error?.message || "Failed to update appointment",
-        severity: "error",
-      }),
+        message: error?.message || 'Failed to update appointment',
+        severity: 'error',
+      })
     );
+  } finally {
+    yield put(hideLoader());
   }
 }
 
 function* deleteAppointmentSaga(action: PayloadAction<number>) {
   try {
-    alert("Deleting appointment with ID: " + action.payload);
-    yield call(
-      DELETE_REQUEST,
-      API_ENDPOINTS.APPOINTMENTS.DELETE(action.payload),
-    );
+    yield put(showLoader());
+    alert('Deleting appointment with ID: ' + action.payload);
+    yield call(DELETE_REQUEST, API_ENDPOINTS.APPOINTMENTS.DELETE(action.payload));
     yield put(deleteAppointmentSuccess(action.payload));
 
     yield put(
       showSnackbar({
-        message: "Appointment deleted successfully",
-        severity: "error",
-      }),
+        message: 'Appointment deleted successfully',
+        severity: 'error',
+      })
     );
 
     yield put(fetchAppointmentsRequest());
   } catch (error: any) {
-    yield put(
-      deleteAppointmentFailure(error.message || "Failed to delete appointment"),
-    );
+    yield put(deleteAppointmentFailure(error.message || 'Failed to delete appointment'));
+  } finally {
+    yield put(hideLoader());
   }
 }
 
--- src/store/patients/saga.ts
+++ src/store/patients/saga.ts
@@ -1,10 +1,5 @@
-import { call, put, takeLatest, all, select } from "redux-saga/effects";
-import {
-  GET_REQUEST,
-  POST_REQUEST,
-  PUT_REQUEST,
-  DELETE_REQUEST,
-} from "../../services/apiHelper";
+import { call, put, takeLatest, all, select } from 'redux-saga/effects';
+import { GET_REQUEST, POST_REQUEST, PUT_REQUEST, DELETE_REQUEST } from '../../services/apiHelper';
 import {
   fetchPatientsRequest,
   fetchPatientsSuccess,
@@ -12,35 +7,33 @@ import {
   addPatientRequest,
   addPatientSuccess,
   addPatientFailure,
-} from "./slice";
-import { PayloadAction } from "@reduxjs/toolkit";
-import { Patient } from "./slice";
-import API_ENDPOINTS from "../../services/apiEndPoint";
-import { selectAppointments } from "./selectors";
-import {
-  isOverlapping,
-  isWithinBusinessHours,
-} from "../../utils/appointment.utils";
-import { showSnackbar } from "../appointments/slice";
+} from './slice';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { Patient } from './slice';
+import API_ENDPOINTS from '../../services/apiEndPoint';
+import { selectAppointments } from './selectors';
+import { isOverlapping, isWithinBusinessHours } from '../../utils/appointment.utils';
+import { showSnackbar } from '../appointments/slice';
+import { hideLoader, showLoader } from '../global/slice';
 
 function* fetchPatientsSaga() {
   try {
-    const response: { data: Patient[] } = yield call(
-      GET_REQUEST,
-      API_ENDPOINTS.PATIENTS.GET_ALL,
-    );
+    yield put(showLoader());
+    const response: { data: Patient[] } = yield call(GET_REQUEST, API_ENDPOINTS.PATIENTS.GET_ALL);
 
-    console.log("Fetched patients saga:", response.data);
+    console.log('Fetched patients saga:', response.data);
 
     yield put(fetchPatientsSuccess(response.data));
   } catch (error: any) {
-    yield put(fetchPatientsFailure("Unable to fetch patients"));
+    yield put(fetchPatientsFailure('Unable to fetch patients'));
     yield put(
       showSnackbar({
-        message: error?.message || "Failed to fetch",
-        severity: "error",
-      }),
+        message: error?.message || 'Failed to fetch',
+        severity: 'error',
+      })
     );
+  } finally {
+    yield put(hideLoader());
   }
 }
 
